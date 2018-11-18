19d3eb901c2989519ae33052d1abe1878b21cb6b
necabo <fynn.becker@gmx.de>
2018-11-18 05:51:19 +0100

    Add state and minor changes

--- python/vimsence.py
+++ python/vimsence.py
@@ -1,30 +1,37 @@
 import vim
 import rpc
 import time
-import logging
+
+
+def get_project_name():
+    return vim.eval('expand("%:p:h:t")')
+
 
 start_time = int(time.time())
+project_name = get_project_name()
 base_activity = {
-        'details': 'Nothing',
+        'details': "Just opened Vim",
+        'state': f"Working on {project_name}",
         'timestamps': {
             "start": start_time
         },
         'assets': {
-            'small_text': 'Vim',
-            'small_image': 'vim_logo',
+            # 'small_text': 'Vim',
+            # 'small_image': 'vim_logo',
             'large_text': 'Vim',
             'large_image': 'vim_logo'
         }
     }
 
-client_id = '425602550470017024'
+client_id = '513185202051350550'
 
 try:
     rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)
     rpc_obj.set_activity(base_activity)
-except Exception as e:
+except Exception:
     # Discord is not running
     pass
+
 
 def update_presence():
     """Update presence in Discord
@@ -32,25 +39,31 @@ def update_presence():
 
     """
     activity = base_activity
-    activity['details'] = get_filename()
-    activity['assets']['large_text'] = 'Editing a {} file'.format(get_extension().upper())
-    if get_extension():
-        activity['assets']['large_image'] = get_extension()
+
+    filename = get_filename()
+    extension = get_extension()
+    if filename:
+        activity['details'] = f"Editing {filename}"
+        activity['assets']['large_text'] = f"Editing a .{get_extension()} file"
+    if extension:
+        activity['assets']['large_image'] = extension
 
     try:
         rpc_obj.set_activity(activity)
-    except BrokenPipeError as e:
+    except BrokenPipeError:
         # Connection to Discord is lost
         pass
-    except NameError as e:
+    except NameError:
         # Discord is not running
         pass
+
 
 def get_filename():
     """Get current filename that is being edited
     :returns: string
     """
     return vim.eval('expand("%:t")')
+
 
 def get_extension():
     """Get exension for file that is being edited
